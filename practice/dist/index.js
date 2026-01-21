899db863c150c767d3f80704e75e092b358876a7
nemuirari <lyukey0902@gmail.com>
2026-01-21 12:15:32 +0900

    Remove node_modules and dist from tracking

--- practice/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// hello world
-const message = "Hello, World!";
-console.log(message);
-//# sourceMappingURL=index.js.map
