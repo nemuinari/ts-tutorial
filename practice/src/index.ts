599cecd212799ad50f49e7fa95352326c5b045ca
nemuirari <lyukey0902@gmail.com>
2026-01-22 12:30:32 +0900

    practice: primitive types

--- practice/src/index.ts
+++ practice/src/index.ts
@@ -1,6 +1,13 @@
-// hello world
-const greeting = "Hello, ";
-const target = "World!";
-const message = greeting + target;
+import { createInterface } from "readline";
 
-console.log(message);
+// primitive types
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Enter a number: ", (line: string) => {
+  const num = Number(line);
+  console.log(num + 1000);
+  rl.close();
+});
