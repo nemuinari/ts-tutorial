992400d4c5b79615e039aee2be0dfc5d12b26636
nemuirari <lyukey0902@gmail.com>
2026-02-04 11:06:56 +0900

    practice: typescript

--- practice/src/index.ts
+++ practice/src/index.ts
@@ -1,21 +1,21 @@
 // chapter 5-1 class declaration
 class User<T> {
   name: string;
-  #age: number;
-  readonly data: T;
+  age: number;
+  data: T;
 
   constructor(name: string, age: number, data: T) {
     this.name = name;
-    this.#age = age;
+    this.age = age;
     this.data = data;
   }
 
   public isAdult(): boolean {
-    return this.#age >= 18;
+    return this.age >= 18;
   }
 }
 
-const uhyo = new User<string>("uhyo", 26, "add data");
+const uhyo: User<string> = new User("uhyo", 26, "add data");
 const data = uhyo.data;
 
 const john = new User("John Smith", 15, { num: 123 });
