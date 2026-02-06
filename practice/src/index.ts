b1d3710d7172c725c4f04a2132185b1caa73aa87
nemuirari <lyukey0902@gmail.com>
2026-02-06 11:52:25 +0900

    practice: typescript

--- practice/src/index.ts
+++ practice/src/index.ts
@@ -1,23 +1,12 @@
-// chapter 5-3 class declaration
-class User {
-  name: string;
-  #age: number; // private field
-
-  constructor(name: string, age: number) {
-    this.name = name;
-    this.#age = age;
-  }
-
-  public isAdult(): boolean {
-    return this.#age >= 20;
-  }
+// chapter 5-4 class declaration
+try {
+  console.log("before error");
+  throwError();
+  console.log("after error");
+} finally {
+  console.log("finally");
 }
 
-const uhyo: User = new User("uhyo", 20);
-const john: User = new User("john", 10);
-
-console.log(uhyo.isAdult()); // true
-
-console.log(uhyo.isAdult.apply(john, [])); // false
-console.log(uhyo.isAdult.call(john)); // false
-console.log(uhyo.isAdult.bind(john)()); // false
+function throwError() {
+  throw new Error("This is an error");
+}
